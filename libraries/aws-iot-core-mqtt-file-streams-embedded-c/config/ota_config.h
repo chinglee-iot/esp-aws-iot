1fbe6b609c2740b9022157a5aec66eca396d4b2b
Ching-Hsin,Lee <chinglee@amazon.com>
2024-06-21 11:05:17 +0800

    Update to LTS 202406 libraries

--- libraries/aws-iot-core-mqtt-file-streams-embedded-c/config/ota_config.h
+++ libraries/aws-iot-core-mqtt-file-streams-embedded-c/config/ota_config.h
@@ -30,6 +30,7 @@
 
 #include "sdkconfig.h"
 #include "MQTTFileDownloader.h"
+#include "MQTTFileDownloader_defaults.h"
 
 #define OTA_DATA_BLOCK_SIZE ( mqttFileDownloader_CONFIG_BLOCK_SIZE )
 #define JOB_DOC_SIZE        ( 2048U )
